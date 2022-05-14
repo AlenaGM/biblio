78000859a1a14afb9f283442facdda3fb3b5fe3f
AlenaGM <alena.guillaume4@gmail.com>
2022-05-14 16:18:53 +0200

    added state to addform

--- src/components/book-add-form/book-add-form.js
+++ src/components/book-add-form/book-add-form.js
@@ -1,23 +1,50 @@
+import { Component } from 'react';
 import './book-add-form.css';
 
-const BookAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Add a new book</h3>
-            <form
-                className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="Title" />
-                <input type="number"
-                    className="form-control new-post-label"
-                    placeholder="Author" />
+class BookAddForm extends Component {
 
-                <button type="submit"
-                        className="btn btn-outline-light">Add</button>
-            </form>
-        </div>
-    )
+    constructor(props) {
+        super(props);
+        this.state = {
+            title:'',
+            author:''
+        }
+    }
+
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+
+    render() {
+        const {title, author} = this.state;
+
+        return (
+            <div className="app-add-form">
+                <h3>Add a new book</h3>
+                <form
+                    className="add-form d-flex">
+                    <input type="text"
+                        className="form-control new-post-label"
+                        placeholder="Title"
+                        name="title"
+                        value={title}
+                        onChange={this.onValueChange}/>
+                    <input type="text"
+                        className="form-control new-post-label"
+                        placeholder="Author"
+                        name="author"
+                        value={author}
+                        onChange={this.onValueChange}/>
+
+                    <button type="submit"
+                            className="btn btn-outline-light">Add</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default BookAddForm;
