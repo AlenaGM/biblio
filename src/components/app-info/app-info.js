f4dd4437102b9bfaa75b96756cc36af72cc6cc37
AlenaGM <alena.guillaume4@gmail.com>
2022-09-27 14:21:59 +0200

    upd img

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,15 +1,15 @@
 import "./app-info.css";
 
-const AppInfo = ({books, wanted, isreading, isfinished}) => {
-    return (
-        <div className="app-info">
-            <h1>Ma Petite Librarie</h1>
-            <h2>Books total: {books}</h2>
-            <h2>Want to read: {wanted}</h2>
-            <h2>Reading now: {isreading}</h2>
-            <h2>Finished to read: {isfinished}</h2>
-        </div>
-    )
-}
+const AppInfo = ({ books, wanted, isreading, isfinished }) => {
+  return (
+    <div className="app-info">
+      <h1>Biblio</h1>
+      <h2>Books total: {books}</h2>
+      <h2>Want to read: {wanted}</h2>
+      <h2>Reading now: {isreading}</h2>
+      <h2>Finished to read: {isfinished}</h2>
+    </div>
+  );
+};
 
 export default AppInfo;
